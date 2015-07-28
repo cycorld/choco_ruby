e2a244183d4c5640ee2954613067f2e4ed42dafd
cycorld <charleschoi87@gmail.com>
2015-07-28 18:33:05 +0900

    이름을 입력 받아 조건을 체크하여 답을 내어 봅시다

--- 5.rb
+++ 5.rb
@@ -2,8 +2,11 @@
 # 조건에 따라 어떤 코드를 실행할지 결정할 수 있습니다
 # 여기서 명제 (참/거짓) 가 매우 중요합니다
 
-if 1 == 1
-  puts "This is true"
+print "What is your name? : "
+name = gets.chomp
+
+if name == "cyc"
+  puts "You are cyc"
 else
-  puts "This is false"
+  puts "Who are you?"
 end
